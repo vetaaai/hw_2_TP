f73cf9778241a241320d342e8671f22cf5dc9586
vetis <stelezhnikova2007@gmail.com>
2026-06-03 13:58:12 +0300

    добавлены пробелы между модулями чтобы удобнее читалось

--- shopping_list.py
+++ shopping_list.py
@@ -3,18 +3,21 @@ from ingredient import Ingredient
 class ShoppingList:
     def __init__(self):
         self._items = []
+        
     def add_recipe(self, recipe, portions):
         if portions <= 0:
             raise ValueError("Количество порций должно быть положительным")
         scaled = recipe.scale(portions)
         for ing in scaled.ingredients:
             self._items.append((ing, recipe.title))
+            
     def remove_recipe(self, title):
         new_items = []
         for item in self._items:
             if item[1] != title:
                 new_items.append(item)
         self._items = new_items
+        
     def get_list(self):
         summary = {}
         for ing, _ in self._items:
@@ -28,6 +31,7 @@ class ShoppingList:
             result.append(Ingredient(name, qty, unit))
         result.sort(key=lambda x: x.name)
         return result
+        
     def __add__(self, other):
         new_list = ShoppingList()
         new_list._items = self._items.copy()
